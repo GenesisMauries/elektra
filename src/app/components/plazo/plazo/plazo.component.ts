4f8fb9aed2fbdf8e26dbd30fc5ffafd09c286447
Génesis Mauries <exodo_54@hotmail.com>
2024-05-31 15:14:19 -0600

    master

---
+++ src/app/components/plazo/plazo/plazo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plazo',
+  standalone: true,
+  imports: [],
+  templateUrl: './plazo.component.html',
+  styleUrl: './plazo.component.css'
+})
+export class PlazoComponent {
+
+}
