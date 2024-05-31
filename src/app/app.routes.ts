f9cbaeac857c601ba280783f250e59be726283ce
Génesis Mauries <exodo_54@hotmail.com>
2024-05-31 16:43:11 -0600

    add router and navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ProductoComponent } from './components/producto/producto/producto.component';
+import { PlazoComponent } from './components/plazo/plazo/plazo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'plazo', component: PlazoComponent },
+{ path: 'producto', component: ProductoComponent }];
---
+++ src/app/components/buttons/swipe-button/swipe-button.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { UpperCasePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-swipe-button',
+  standalone: true,
+  imports: [UpperCasePipe],
+  templateUrl: './swipe-button.component.html',
+  styleUrl: './swipe-button.component.css'
+})
+export class SwipeButtonComponent {
+  currentOption: string = 'Productos';
+
+  constructor(private router: Router, private location: Location) {}
+
+  toggleOption() {
+    const currentPath = this.location.path(false); // Get sanitized path
+    const navigateTo = currentPath === '/producto' ? '/plazo' : '/producto';
+
+    this.router.navigate([navigateTo]);
+    this.currentOption = navigateTo === '/producto' ? 'Productos' : 'Plazo'; // Update text based on navigation
+  }
+}
